59504f6d58503de7f267a0ec88ca6bf9a9bcf5a9
Daniel Egger <daniel@eggers-club.de>
2018-03-23 08:40:56 +0100

    Added small tool script capture_example_bloat to record bloat for all examples
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

---
+++ tools/capture_example_bloat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+filename="bloat_log_"`date -Iminutes`".txt"
+
+for i in `find examples -name "*.rs"`; do
+        name=$(echo $i | sed -e "s,examples/,,g" -e "s,\.rs,,g")
+        echo "Processing example $name"
+        echo >>$filename
+        echo "Bloat for example $name" >>$filename
+        xargo bloat --release --example $name >>$filename
+done
+
+echo "Captures bloat for all examples into $filename"
