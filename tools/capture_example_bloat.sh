5b1241d633d5c7e3e6694c6e53f80b923839fa5b
Daniel Egger <daniel@eggers-club.de>
2018-05-12 23:26:16 +0200

    Update bloat script to use cargo instead of xargo
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

--- tools/capture_example_bloat.sh
+++ tools/capture_example_bloat.sh
@@ -7,7 +7,7 @@ for i in `find examples -name "*.rs"`; do
         echo "Processing example $name"
         echo >>$filename
         echo "Bloat for example $name" >>$filename
-        xargo bloat --release --example $name >>$filename
+        cargo bloat --release --example $name >>$filename
 done
 
 echo "Captures bloat for all examples into $filename"
