42cedcaead4d76d65f652ede527a930b94248ad7
Jeremy Stucki <dev@jeremystucki.ch>
2020-02-15 14:57:33 +0100

    Use 'into' instead of 'downgrade'

--- examples/i2c_haldriver_printmagserial.rs
+++ examples/i2c_haldriver_printmagserial.rs
@@ -47,8 +47,8 @@ fn main() -> ! {
             let gpio = p.GPIO.split();
 
             /* Configure RX and TX pins accordingly */
-            let tx = gpio.pin24.into_push_pull_output().downgrade();
-            let rx = gpio.pin25.into_floating_input().downgrade();
+            let tx = gpio.pin24.into_push_pull_output().into();
+            let rx = gpio.pin25.into_floating_input().into();
 
             /* Set up serial port using the prepared pins */
             let (mut tx, _) = serial::Serial::uart0(p.UART0, tx, rx, BAUD115200).split();
@@ -57,8 +57,8 @@ fn main() -> ! {
             *TX.borrow(cs).borrow_mut() = Some(tx);
 
             /* Configure SCL and SDA pins accordingly */
-            let scl = gpio.pin0.into_open_drain_input().downgrade();
-            let sda = gpio.pin30.into_open_drain_input().downgrade();
+            let scl = gpio.pin0.into_open_drain_input().into();
+            let sda = gpio.pin30.into_open_drain_input().into();
 
             /* Set up I2C */
             let i2c = i2c::I2c::i2c1(p.TWI1, sda, scl);
