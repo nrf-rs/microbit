7337b89a560ce650ace08815bb96e35fe9969d3c
Henrik Alsér <henrik@mindbite.se>
2021-07-01 12:39:54 +0200

    Add BLE example

---
+++ examples/ble-beacon/src/main.rs
@@ -0,0 +1,60 @@
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+use embedded_hal::blocking::delay::DelayMs;
+use microbit::hal::{clocks::Clocks, timer::Timer};
+use rubble::beacon::Beacon;
+use rubble::link::{ad_structure::AdStructure, CompanyId, MIN_PDU_BUF};
+use rubble_nrf5x::radio::{BleRadio, PacketBuffer};
+use rubble_nrf5x::utils::get_device_address;
+
+#[entry]
+fn main() -> ! {
+    static mut BLE_TX_BUF: PacketBuffer = [0; MIN_PDU_BUF];
+    static mut BLE_RX_BUF: PacketBuffer = [0; MIN_PDU_BUF];
+    if let Some(p) = microbit::Peripherals::take() {
+        // On reset, the internal high frequency clock is already used, but we
+        // also need to switch to the external HF oscillator. This is needed
+        // for Bluetooth to work.
+        let _clocks = Clocks::new(p.CLOCK).enable_ext_hfosc();
+
+        // Determine device address
+        let device_address = get_device_address();
+
+        // Rubble currently requires an RX buffer even though the radio is only used as a TX-only beacon.
+        let mut radio = BleRadio::new(p.RADIO, &p.FICR, BLE_TX_BUF, BLE_RX_BUF);
+
+        let mut timer = Timer::new(p.TIMER0);
+
+        loop {
+            defmt::info!("Local name");
+            let beacon = Beacon::new(
+                device_address,
+                &[AdStructure::CompleteLocalName("Rusty microbit")],
+            )
+            .unwrap();
+            beacon.broadcast(&mut radio);
+            timer.delay_ms(1_000_u16);
+
+            let data = "Hello world";
+            defmt::info!("{}", data);
+            let beacon = Beacon::new(
+                device_address,
+                &[AdStructure::ManufacturerSpecificData {
+                    company_identifier: CompanyId::from_raw(0xffff),
+                    payload: data.as_bytes(),
+                }],
+            )
+            .unwrap();
+            beacon.broadcast(&mut radio);
+            timer.delay_ms(1_000_u16);
+        }
+    }
+
+    loop {
+        continue;
+    }
+}
