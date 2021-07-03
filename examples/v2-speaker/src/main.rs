9ee4f8bb4d4e725c64b3e923a0324fdfd530a65e
Erik Svensson <erik.public@gmail.com>
2021-07-03 23:19:57 +0200

    Documentation and some small changes

--- examples/v2-speaker/src/main.rs
+++ examples/v2-speaker/src/main.rs
@@ -20,12 +20,11 @@ static SPEAKER: Mutex<RefCell<Option<pwm::Pwm<pac::PWM0>>>> = Mutex::new(RefCell
 fn main() -> ! {
     if let Some(p) = microbit::Peripherals::take() {
         cortex_m::interrupt::free(move |cs| {
+            // NB: The LF CLK pin is used by the speaker
             let _clocks = Clocks::new(p.CLOCK)
                 .enable_ext_hfosc()
                 .set_lfclk_src_synth()
                 .start_lfclk();
-
-            defmt::info!("Speaker");
 
             p.RTC0.prescaler.write(|w| unsafe { w.bits(511) });
             p.RTC0.evtenset.write(|w| w.tick().set_bit());
@@ -36,27 +35,36 @@ fn main() -> ! {
 
             let p0parts = gpio::p0::Parts::new(p.P0);
 
-            let mut speaker_pin = p0parts.p0_00.into_push_pull_output(gpio::Level::Low);
+            let mut speaker_pin = p0parts.p0_00.into_push_pull_output(gpio::Level::High);
             let _ = speaker_pin.set_low();
 
+            // Use the PWM peripheral to generate a waveform for the speaker
             let speaker = pwm::Pwm::new(p.PWM0);
             speaker
+                // output the waveform on the speaker pin
                 .set_output_pin(pwm::Channel::C0, &speaker_pin.degrade())
+                // Use prescale by 16 to achive darker sounds
                 .set_prescaler(pwm::Prescaler::Div16)
-                .set_period(Hertz(1000u32))
+                // Initial frequency
+                .set_period(Hertz(1u32))
+                // Configure for up and down counter mode
                 .set_counter_mode(pwm::CounterMode::UpAndDown)
+                // Set maximum duty cycle
+                .set_max_duty(32767)
+                // enable PWM
                 .enable();
+
             speaker
                 .set_seq_refresh(pwm::Seq::Seq0, 0)
-                .set_seq_refresh(pwm::Seq::Seq1, 0)
-                .set_seq_end_delay(pwm::Seq::Seq0, 0)
-                .set_seq_end_delay(pwm::Seq::Seq1, 0);
+                .set_seq_end_delay(pwm::Seq::Seq0, 0);
 
+            // Configure 50% duty cycle
             let max_duty = speaker.max_duty();
             speaker.set_duty_on_common(max_duty / 2);
 
             *SPEAKER.borrow(cs).borrow_mut() = Some(speaker);
 
+            // Configure RTC interrupt
             unsafe {
                 pac::NVIC::unmask(pac::Interrupt::RTC0);
             }
@@ -69,25 +77,40 @@ fn main() -> ! {
     }
 }
 
-// Define an exception, i.e. function to call when exception occurs. Here if our timer
-// trips, we'll print some random number
+const STOP_FREQUENCY: u32 = 500;
+
+// RTC interrupt, exectued for each RTC tick
 #[interrupt]
 fn RTC0() {
     static mut FREQUENCY: u32 = 1;
     /* Enter critical section */
     cortex_m::interrupt::free(|cs| {
-        if let Some(speaker) = SPEAKER.borrow(cs).borrow().as_ref() {
-            speaker.set_period(Hertz(*FREQUENCY));
-            defmt::info!("Speaker {}", *FREQUENCY);
+        /* Borrow devices */
+        if let (Some(speaker), Some(rtc)) = (
+            SPEAKER.borrow(cs).borrow().as_ref(),
+            RTC.borrow(cs).borrow().as_ref(),
+        ) {
+            if *FREQUENCY < STOP_FREQUENCY {
+                // Configure the new frequency, must not be zero.
+                // Will change the max_duty
+                speaker.set_period(Hertz(*FREQUENCY));
+            } else {
+                // Continue at frequency
+                speaker.set_period(Hertz(STOP_FREQUENCY));
+            }
+            // Restart the PWM at 50% duty cycle
             let max_duty = speaker.max_duty();
             speaker.set_duty_on_common(max_duty / 2);
-        }
-        if let Some(rtc) = RTC.borrow(cs).borrow().as_ref() {
+            if *FREQUENCY >= STOP_FREQUENCY + 250 {
+                defmt::info!("Fin");
+                // Stop speaker and RTC
+                speaker.disable();
+                rtc.tasks_stop.write(|w| unsafe { w.bits(1) });
+            };
+            // Clear the RTC interrupt
             rtc.events_tick.write(|w| unsafe { w.bits(0) });
         }
     });
+    // Increase the frequency
     *FREQUENCY += 1;
-    if *FREQUENCY >= 1000 {
-        *FREQUENCY = 1
-    };
 }
