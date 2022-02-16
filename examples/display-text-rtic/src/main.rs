ddf75051930bdc5db9856659c714d3ea361e80e7
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-02-16 22:47:16 +0100

    Port display-text-rtic example to RTIC 1.0

--- examples/display-text-rtic/src/main.rs
+++ examples/display-text-rtic/src/main.rs
@@ -18,21 +18,28 @@ use microbit::{
 };
 use microbit_text::scrolling::Animate;
 use microbit_text::scrolling_text::ScrollingStaticText;
-
 use rtic::app;
 
 const MESSAGE: &[u8] = b"Hello, world!";
 
 #[app(device = microbit::pac, peripherals = true)]
-const APP: () = {
-    struct Resources {
+mod app {
+    use super::*;
+
+    #[shared]
+    struct Shared {
         display: Display<pac::TIMER1>,
         anim_timer: Rtc<pac::RTC0>,
         scroller: ScrollingStaticText,
     }
 
+    #[local]
+    struct Local {
+        frame: MicrobitFrame,
+    }
+
     #[init]
-    fn init(cx: init::Context) -> init::LateResources {
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
         let board = Board::new(cx.device, cx.core);
 
         // Starting the low-frequency clock (needed for RTC to work)
@@ -50,29 +57,34 @@ const APP: () = {
         let mut scroller = ScrollingStaticText::default();
         scroller.set_message(MESSAGE);
 
-        init::LateResources {
-            anim_timer: rtc0,
-            display,
-            scroller,
-        }
+        (
+            Shared {
+                anim_timer: rtc0,
+                display,
+                scroller,
+            },
+            Local {
+                frame: MicrobitFrame::default(),
+            },
+            init::Monotonics(),
+        )
     }
 
-    #[task(binds = TIMER1, priority = 2, resources = [display])]
-    fn timer1(cx: timer1::Context) {
-        cx.resources.display.handle_display_event();
+    #[task(binds = TIMER1, priority = 2, shared = [display])]
+    fn timer1(mut cx: timer1::Context) {
+        cx.shared.display.lock(|d| d.handle_display_event());
     }
 
-    #[task(binds = RTC0, priority = 1,
-           resources = [anim_timer, display, scroller])]
+    #[task(binds = RTC0, priority = 1, shared = [anim_timer, display, scroller], local = [frame])]
     fn rtc0(mut cx: rtc0::Context) {
-        static mut FRAME: MicrobitFrame = MicrobitFrame::default();
-        cx.resources.anim_timer.reset_event(RtcInterrupt::Tick);
-        if !cx.resources.scroller.is_finished() {
-            cx.resources.scroller.tick();
-            FRAME.set(cx.resources.scroller);
-            cx.resources.display.lock(|display| {
-                display.show_frame(FRAME);
-            });
+        cx.shared
+            .anim_timer
+            .lock(|t| t.reset_event(RtcInterrupt::Tick));
+        if !cx.shared.scroller.lock(|s| s.is_finished()) {
+            cx.shared.scroller.lock(|s| s.tick());
+            let frame = cx.local.frame;
+            cx.shared.scroller.lock(|s| frame.set(s));
+            cx.shared.display.lock(|display| display.show_frame(frame));
         }
     }
-};
+}
