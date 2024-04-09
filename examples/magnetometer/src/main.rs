213d42bb9ac91ca83b0b7d7e9b9253fad4de95f6
Andrew Walbran <qwandor@google.com>
2024-04-09 16:20:35 +0100

    Update lsm303agr driver in example.

--- examples/magnetometer/src/main.rs
+++ examples/magnetometer/src/main.rs
@@ -34,25 +34,37 @@ fn main() -> ! {
     let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
 
     let mut sensor = Lsm303agr::new_with_i2c(i2c);
-    match sensor.accelerometer_id() {
-        Ok(0x33u8) => {}
-        _ => defmt::panic!("accelerometer not found"),
+    if let Ok(id) = sensor.accelerometer_id() {
+        if !id.is_correct() {
+            defmt::panic!("Accelerometer had unexpected ID {:#x}", id.raw());
+        }
+    } else {
+        defmt::panic!("Error getting accelerometer ID");
     }
     sensor.init().unwrap();
-    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
 
     defmt::info!("normal mode");
-    sensor.set_accel_mode(AccelMode::Normal).unwrap();
+    sensor
+        .set_accel_mode_and_odr(&mut timer, AccelMode::Normal, AccelOutputDataRate::Hz50)
+        .unwrap();
     timer.delay_ms(1000_u32);
     get_data(&mut sensor);
 
     defmt::info!("low power mode");
-    sensor.set_accel_mode(AccelMode::LowPower).unwrap();
+    sensor
+        .set_accel_mode_and_odr(&mut timer, AccelMode::LowPower, AccelOutputDataRate::Hz50)
+        .unwrap();
     timer.delay_ms(1000_u32);
     get_data(&mut sensor);
 
     defmt::info!("high resolution mode");
-    sensor.set_accel_mode(AccelMode::HighResolution).unwrap();
+    sensor
+        .set_accel_mode_and_odr(
+            &mut timer,
+            AccelMode::HighResolution,
+            AccelOutputDataRate::Hz50,
+        )
+        .unwrap();
     timer.delay_ms(1000_u32);
     get_data(&mut sensor);
 
@@ -70,9 +82,9 @@ type Sensor = Lsm303agr<I2cInterface<twim::Twim<TWIM0>>, MagOneShot>;
 
 fn get_data(sensor: &mut Sensor) {
     loop {
-        if sensor.accel_status().unwrap().xyz_new_data {
-            let data = sensor.accel_data().unwrap();
-            defmt::info!("x {} y {} z {}", data.x, data.y, data.z);
+        if sensor.accel_status().unwrap().xyz_new_data() {
+            let data = sensor.acceleration().unwrap();
+            defmt::info!("x {} y {} z {}", data.x_mg(), data.y_mg(), data.z_mg());
             return;
         }
     }
